bab632d3ce9a3b67477a64613ba2d948ab36f8d8
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2022-06-23 19:39:16 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../style.css'
+import Head from "next/head";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+  <div>
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
+)}

c0abe1dc351abd18f3a510e04fd3345f8aed8f44
Vishakha Aggarwal <76397035+vishakha-aggarwal@users.noreply.github.com>
2022-06-25 01:08:23 +0530

    initial commit

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,15 +1,21 @@
-const Web3 = require("web3");
-let web3;
+import Web3 from 'web3';
 
-if(typeof window !== 'undefined' && window.web3 !== 'undefined'){
-  // We are on a browser and metamask is running
-  web3 = new Web3(window.web3.currentProvider);
-}else{
-  // We are on the Server OR the user isn't runnig metamask
-  const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/4d539ad5da44453595cc1d281731878d'
-  );
-  web3 = new Web3(provider);
+var web3;
+
+if (typeof window !== 'undefined') {
+    if(window.web3 !== 'undefined'){
+        const provider = new Web3.providers.HttpProvider(
+            'https://rinkeby.infura.io/v3/4d539ad5da44453595cc1d281731878d'
+            );
+        web3 = new Web3(provider);
+    } else{
+        web3 = new Web3(window.web3.currentProvider);
+    }
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/4d539ad5da44453595cc1d281731878d'
+        );
+    web3 = new Web3(provider);
 }
 
-module.exports = web3;
+export default web3;
